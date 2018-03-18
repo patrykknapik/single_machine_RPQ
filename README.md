4c21c50faefc71746a90a8e55486e15e2ff6fe5b
Patryk Knapik <226302@student.pwr.edu.pl>
2018-03-18 01:40:00 +0100

    README updated

--- README.md
+++ README.md
@@ -8,5 +8,17 @@ You may want to change:
 1. Number of tasks by changing `TASK_COUNT`
 2. Minimal time for step by changing `MIN_TIME`
 3. Maximal time for step by changing `MAX_TIME`
+4. You can save some time by turning of `SHOW_PERMUTATIONS`
+5. I used `GENERATE_TEST_DATA` to generate below tables
 
+###Test times
+
+#####Two Machines
+| Number of tasks: |sortR  | make span | Brute force | make span|
+| ------------- |-------------:| -----:| -----:| -----:|
+
+
+###Summary
+SortR is not perfect but is significantly faster.
+ 
 *for SPD lab pt11 on PWR*
--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <sys/time.h>
+#include <fstream>
 #include "task.h"
 
-#define TASK_COUNT 5
+#define TASK_COUNT 13
 #define MIN_TIME 1
 #define MAX_TIME 20
+#define SHOW_PERMUTATIONS false
+#define GENRATE_TEST_DATA true
 
 unsigned long int factorial(unsigned long int i) {
     unsigned long int f = 1;
@@ -18,7 +21,7 @@ unsigned long int factorial(unsigned long int i) {
 
 int main() {
     timeval time_stamp_start, time_stamp_stop;
-    double elapsed_time;
+    double elapsed_time, bruteForceTime, sortRTime;
     std::vector<task *> tasks;
     std::vector<task *> queuedTasks;
     unsigned long int tasksPermutations = 0;
@@ -38,19 +41,39 @@ int main() {
 
     //brute force:
     std::cout << "\n\nBrute force algorithm:\n";
+    if(SHOW_PERMUTATIONS) {
+        tasksPermutations = factorial(tasks.size());
+        std::cout << "\nPermutations(" << tasksPermutations << "):\n";
+
+        for (; tasksPermutations != 0; --tasksPermutations) {
+            std::next_permutation(tasks.begin(), tasks.end());
+
+            for (task *Task : tasks) {
+                std::cout << Task->getID() << ' ';
+                t = std::max(Task->getR(), t) + Task->getP();
+                Cmax = std::max(Cmax, (t + Task->getQ()));
+            }
+            std::cout << "; C = " << Cmax << '\n';
+
+            if (Cmax < Cmin) {
+                Cmin = Cmax;
+                queuedTasks = tasks;
+            }
+
+            Cmax = 0;
+            t = 0;
+        }
+    }
+
     gettimeofday(&time_stamp_start, NULL);
     tasksPermutations = factorial(tasks.size());
-    std::cout << "\nPermutations(" << tasksPermutations << "):\n";
-
     for (; tasksPermutations != 0; --tasksPermutations) {
         std::next_permutation(tasks.begin(), tasks.end());
 
         for (task *Task : tasks) {
-            std::cout << Task->getID() << ' ';
             t = std::max(Task->getR(), t) + Task->getP();
             Cmax = std::max(Cmax, (t + Task->getQ()));
         }
-        std::cout << "; C = " << Cmax << '\n';
 
         if (Cmax < Cmin) {
             Cmin = Cmax;
@@ -70,11 +93,11 @@ int main() {
     elapsed_time = (time_stamp_stop.tv_sec - time_stamp_start.tv_sec) * 1000.0;      // sec to ms
     elapsed_time += (time_stamp_stop.tv_usec - time_stamp_start.tv_usec) / 1000.0;   // us to ms
     std::cout << "Computation time: " << elapsed_time << " ms.\n\n\n";
-
+    bruteForceTime = elapsed_time;
 
     //sortR:
     std::cout << "SortR algorithm:\n";
-    std::cout << "Sugested tasks queue: ";
+    std::cout << "\nSugested tasks queue: ";
     gettimeofday(&time_stamp_start, NULL);
     std::sort(tasks.begin(),tasks.end(),sortRcomp);
     for (task *Task : tasks) {
@@ -88,6 +111,15 @@ int main() {
     elapsed_time = (time_stamp_stop.tv_sec - time_stamp_start.tv_sec) * 1000.0;      // sec to ms
     elapsed_time += (time_stamp_stop.tv_usec - time_stamp_start.tv_usec) / 1000.0;   // us to ms
     std::cout << "Computation time: " << elapsed_time << " ms.\n\n\n";
+    sortRTime = elapsed_time;
+
+    if(GENRATE_TEST_DATA){
+        std::ofstream file;
+        file.open("test_data.log",std::ofstream::app);
+        file << "|" << TASK_COUNT << "|" << sortRTime << " ms|"<< Cmax;
+        file<< "|" << bruteForceTime <<" ms|"<< Cmin << "|\n";
+        file.close();
+    }
 
     for (task *Task : tasks) {
         delete Task;
